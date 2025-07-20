2b3329542e6d3b7d9877d29ba5bf362880ce17e7
xDarey <oluwadare.emmanuel15@gmail.com>
2025-07-20 17:24:19 +0100

    24/7

--- app/services/24-7-support/page.tsx
+++ app/services/24-7-support/page.tsx
@@ -85,7 +85,7 @@ export default function SupportPage() {
               <div className="relative">
                 <div className="aspect-[4/3] rounded-2xl overflow-hidden shadow-2xl">
                   <Image
-                    src="/images/gapp4.jpg"
+                    src="/images/24:7nurse.avif"
                     alt=" professional care team"
                     width={600}
                     height={450}
